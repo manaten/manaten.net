1ee1f9cb8d1dabae56f0e3d9fe7cf0cb295535ab
manaten <manaten@manaten.net>
2017-08-27 22:19:09 +0900

    chore(scripts): † fix imagemin script. †

--- scripts/imagemin.js
+++ scripts/imagemin.js
@@ -1,19 +1,15 @@
-#!/usr/bin/env node
 'use strict';
 const imagemin = require('imagemin');
 const fs = require('fs');
 
-const DEFAULT_PLUGINS = [
-  'gifsicle',
-  'jpegtran',
-  'optipng',
-  'svgo'
-];
-const requirePlugins = plugins => plugins.map(x => require(`imagemin-${x}`)());
-
 (async () => {
-  const input = process.argv.filter(arg => /(png|jpe?g|gif)/.test(arg));
-  const plugins = requirePlugins(DEFAULT_PLUGINS);
+  const input = process.argv.filter(arg => /(png|jpe?g|gif|svg)/.test(arg));
+  const plugins = [
+    'gifsicle',
+    'jpegtran',
+    'optipng',
+    'svgo'
+  ].map(x => require(`imagemin-${x}`)());
 
   for (const inputPath of input) {
     const outputData = (await imagemin([inputPath], {plugins}))[0].data;
