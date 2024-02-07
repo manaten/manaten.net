399c77d8b82b16c0d52a6a9fb5300a047295e50d
manaten <586064+manaten@users.noreply.github.com>
2024-02-08 01:48:38 +0900

    feat: すごい今更ながらogpを実装 (#21)
    * feat: すごい今更ながらogpを実装
    
    * fix: 画像をちゃんと別で用意

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,74 @@
-const webpack = require('webpack');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const PATHS = {
-  src : path.join(__dirname, 'src'),
-  dist: path.join(__dirname, 'dist')
+  src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist'),
 };
 
 const config = {
-  context  : PATHS.src,
-  entry    : [
+  context: PATHS.src,
+  entry: [
     './index.pug',
     './gallery/index.pug',
     './assets/css/hatena-blog-theme.css',
   ],
-  output   : {
-    path      : PATHS.dist,
+  output: {
+    path: PATHS.dist,
     publicPath: '/',
-    filename  : '.dummy'
+    filename: '.dummy',
   },
-  module   : {
+  module: {
     rules: [
       {
-        test   : /\.(gif|png|ico|eot|svg|ttf|woff)$/,
+        test: /\.(gif|png|ico|eot|svg|ttf|woff)$/,
         exclude: /\.embed\.(gif|png)$/,
-        use    : 'file-loader?name=[path][name].[ext]'
+        use: 'file-loader?name=[path][name].[ext]',
       },
       {
         test: /\.embed\.(gif|png)$/,
-        use : 'url-loader'
+        use: 'url-loader',
       },
       {
         test: /\.pug$/,
-        use : [
+        use: [
           'file-loader?name=[path][name].html',
           'extract-loader',
           `html-loader?attrs=img:src link:href&root=${PATHS.src}`,
-          'pug-html-loader?exports=false'
-        ]
+          'pug-html-loader?exports=false',
+        ],
       },
       {
         test: /\.css$/,
-        use : [
+        use: [
           'file-loader?name=[path][name].css',
           'extract-loader',
           `css-loader?minimize&root=${PATHS.src}`,
           {
-            loader : 'postcss-loader',
+            loader: 'postcss-loader',
             options: {
               plugins: [
                 require('postcss-smart-import')(),
-                require('postcss-cssnext')()
-              ]
-            }
-          }
-        ]
-      }
+                require('postcss-cssnext')(),
+              ],
+            },
+          },
+        ],
+      },
     ],
   },
-  devtool  : 'source-map',
-  plugins  : [
+  devtool: 'source-map',
+  plugins: [
     new CleanWebpackPlugin([PATHS.dist]),
     new CopyWebpackPlugin([
-      {from: 'blog-entries/images', to: 'wp-content/uploads'}
-    ])
+      { from: 'blog-entries/images', to: 'wp-content/uploads' },
+      { from: 'assets/images/og.gif', to: 'assets/images/og.gif' },
+    ]),
   ],
   devServer: {
-    host       : '',
-    port       : process.env.PORT || '8080',
+    host: '',
+    port: process.env.PORT || '8080',
     contentBase: PATHS.dist,
   },
 };
